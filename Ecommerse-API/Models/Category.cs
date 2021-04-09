64fcbf617488f83d9b8ded49b5bccc2f4fe2ccfc
shweeta-s <79075667+shweeta-s@users.noreply.github.com>
2021-04-09 18:10:50 +0530

    Add files via upload

---
+++ Ecommerse-API/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prodo.API.Models
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ Ecommerse-API/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prodo.API.Models
+{
+    public class Product : BaseEntity
+    {
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Cost { get; set; }
+
+        public bool OutOfStock { get; set; }
+    }
+}
